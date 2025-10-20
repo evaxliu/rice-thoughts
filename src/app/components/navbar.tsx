c61d71eddbac9b2c39b12b43bef31e73258995a9
evaxliu <evaxinerliu@gmail.com>
2025-10-19 22:55:06 -0700

    sticky nav bar attempt

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -11,6 +11,7 @@ import React from "react";
 import {
   IconButton,
   Collapse,
+  Navbar,
 } from "@material-tailwind/react";
 
 export default function App() {
@@ -34,19 +35,19 @@ export default function App() {
 
   return (
     <div>
-      <header className="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full text-sm py-3">
-        <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
-          <div className="flex items-center justify-between">
-            <Link className="flex-none text-xl font-semibold focus:outline-hidden" href="/" aria-label="Rice Thoughts">
+      <Navbar className="bg-transparent border-transparent sticky top-0 z-50 backdrop-blur-md border-b mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
+        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+          <div className="flex items-center justify-between py-1.5">
+            <Link className="flex-none" href="/" aria-label="Rice Thoughts">
               <span className="inline-flex items-center gap-x-2 text-xl font-semibold">
-                <Image className="w-10 h-auto" src={icon} alt="Rice Thoughts Logo" />
+                <Image className="w-10" src={icon} alt="Rice Thoughts Logo"/>
                 Rice Thoughts
               </span>
             </Link>
           </div>
-
-          {/* Desktop */}
-          <div className="hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+          
+          {/* Desktop and Tablets */}
+          <div className="hidden sm:block">
             <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
               {navItems.map((item) => (
                 <Link key={item.label} aria-current="page" color="secondary" href={item.href} className={`${
@@ -69,63 +70,155 @@ export default function App() {
                 type="search"
               />
             </div>
+          </div>
+          
+          {/* Trigger Mobile Nav Dropdown */}
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent sm:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
 
-            {/* Tablet and Mobile */}
-            <div className="block sm:hidden transition-all duration-300 basis-full grow">
-              <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-                <IconButton
-                  variant="text"
-                  className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                  ripple={false}
-                  onClick={() => setOpenNav(!openNav)}
-                >
-                  {openNav ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      className="h-6 w-6"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
-                </IconButton>
-                <Collapse open={openNav}>
-                  <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-                    {navItems.map((item) => (
-                      <Link key={item.label} aria-current="page" color="secondary" href={item.href} className={`${
-                        isActive(item.href) ? "text-purple-500" : ""
-                      }`}>
-                        {item.label}
-                      </Link>
-                    ))}
-                  </div>
-                </Collapse>
-              </div>
+        {/* Mobile */}
+        <Collapse open={openNav}>
+          <div className="container mx-auto">
+            <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+              {navItems.map((item) => (
+                <Link key={item.label} aria-current="page" color="secondary" href={item.href} className={`${
+                  isActive(item.href) ? "text-purple-500" : ""
+                }`}>
+                  {item.label}
+                </Link>
+              ))}
+              <Input
+                classNames={{
+                  base: "max-w-full sm:max-w-[10rem] h-10",
+                  mainWrapper: "h-full",
+                  input: "text-small",
+                  inputWrapper:
+                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+                }}
+                placeholder="Type to search..."
+                size="sm"
+                startContent={<SearchIcon size={18} />}
+                type="search"
+              />
             </div>
           </div>
-        </nav>
-      </header>
+        </Collapse>
+      </Navbar>
+ 
+    <div className="">
+      <Navbar className="bg-transparent border-transparent sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <div className="flex items-center justify-between py-1.5">
+            <Link className="flex-none" href="/" aria-label="Rice Thoughts">
+              <span className="inline-flex items-center gap-x-2 text-xl font-semibold">
+                <Image className="w-10" src={icon} alt="Rice Thoughts Logo"/>
+                Rice Thoughts
+              </span>
+            </Link>
+          </div>
+
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent sm:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
+        <Collapse open={openNav}>
+          <div className="container mx-auto">
+            <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+              {navItems.map((item) => (
+                <Link key={item.label} aria-current="page" color="secondary" href={item.href} className={`${
+                  isActive(item.href) ? "text-purple-500" : ""
+                }`}>
+                  {item.label}
+                </Link>
+              ))}
+              <Input
+                classNames={{
+                  base: "max-w-full sm:max-w-[10rem] h-10",
+                  mainWrapper: "h-full",
+                  input: "text-small",
+                  inputWrapper:
+                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+                }}
+                placeholder="Type to search..."
+                size="sm"
+                startContent={<SearchIcon size={18} />}
+                type="search"
+              />
+            </div>
+          </div>
+        </Collapse>
+      </Navbar>
+      </div>
     </div>
   );
 }
