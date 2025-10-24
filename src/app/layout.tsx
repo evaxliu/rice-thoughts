c94d0dc03c8da69c8c6d1913161d0792bf7ef76a
evaxliu <evaxinerliu@gmail.com>
2025-10-24 14:21:15 -0700

    twitter card and open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,29 @@ export const metadata = {
       { url: "/Kengdoru.png", sizes: "32x32", type: "image/png" },
     ]
   },
+    twitter: {
+    card: "summary_large_image",
+    title: "Rice Thoughts Blog",
+    description:
+      "A guy who likes rice and thinks. A prose on food, society and politics.",
+    images: ["https://ricethoughts.com/Kengdoru.png"],
+    creator: "@YourHandle",
+  },
+  openGraph: {
+    title: "Rice Thoughts Blog",
+    description:
+      "A guy who likes rice and thinks. A prose on food, society and politics.",
+    url: "https://ricethoughts.com",
+    siteName: "Rice Thoughts Blog",
+    images: [
+      {
+        url: "https://ricethoughts.com/Kengdoru.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
