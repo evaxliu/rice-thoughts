5fd281ef2fd9b4938b703364fdccd022148cc5a7
evaxliu <evaxinerliu@gmail.com>
2025-10-23 18:55:51 -0700

    icon metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,12 @@ import ClientLayout from "./client-layout";
 export const metadata = {
   title: "Rice Thoughts Blog",
   description: "A guy who likes rice and thinks. An essay on culture and politics.",
-  icon: "./favicon.ico"
+  icons: {
+    icon: [
+      { url: "../../public/favicon.ico" },
+      { url: "../../public/Kengdoru.png", sizes: "32x32", type: "image/png" },
+    ]
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
