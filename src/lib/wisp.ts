eb2c5460f6547f73c7fe537deaad05dfefd6c996
Eva L <69877857+evaxliu@users.noreply.github.com>
2026-03-09 00:52:21 -0700

    Update wisp.ts

--- src/lib/wisp.ts
+++ src/lib/wisp.ts
@@ -5,7 +5,7 @@ import {
 } from "@wisp-cms/client";
 
 export const wisp = buildWispClient({
-  blogId: "83f9f471-9804-41ce-8e32-b70c442c75d7",
+  blogId: "10eac2c0-5873-49de-b7e7-55d3a5602357",
 });
 
 export type { GetPostsResult, GetPostResult };
